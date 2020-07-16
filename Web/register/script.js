302df7d68c3c69bdf48b5172c157ca6a79bc79f5
paul-hanneforth <paul.hanneforth.o@gmail.com>
2020-07-16 15:17:42 +0200

    refactoring

--- Web/register/script.js
+++ Web/register/script.js
@@ -1,46 +1,10 @@
-document.querySelector("#login-button").href = "/login" + window.location.search;
+const run = async () => {
 
-const emailInput = document.getElementById("email")
-const passwordInput = document.getElementById("password")
-const nameInput = document.getElementById("name");
-const button = document.getElementById("register-button")
+    const box = document.getElementById("box");
+    const viewTemplate = await fetchView("views/register.html");
+    const view = await loadView(viewTemplate);
 
-const action = queryParameter("action");
-const app = queryParameter("app");
-const redirectURL = queryParameter("redirect");
+    renderView(box, view);
 
-const displayError = (text) => {
-    const textbox = document.querySelector(".text-box");
-    textbox.classList.remove("invisible");
-    textbox.querySelector("span").innerHTML = text;
 }
-
-button.addEventListener("click", async () => {
-
-    const email = emailInput.value == "" ? null : emailInput.value;
-    const password = passwordInput.value == "" ? null : passwordInput.value;
-    const name = nameInput.value == "" ? null : nameInput.value;
-
-    const jsonResult = await request(serverAddress + "/user/create", {
-        email,
-        password,
-        name
-    })
-    if(jsonResult.error != false) { 
-        displayError(jsonResult.error.message);
-        return;
-    }
-
-    setCookie("userAccessToken", jsonResult.token, 30);
-
-    if(action == "authenticate") {
-        const result = await addUserToApp(tokenRequest.token, app);
-        window.location.href = redirectURL + "?authToken=" + result.authToken
-    } else if(action == "login-redirect") {
-        window.location.href = redirectURL;
-    } else {
-        window.location.href = serverAddress + "/user";
-    }
-    return;
-
-})
+run();
